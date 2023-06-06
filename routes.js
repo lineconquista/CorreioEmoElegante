1098d3942b6e3d3be2760c8852964bd4593e90ea
Aline Conquista <aline.conquista@grupoboticario.com.br>
2023-06-05 22:37:27 -0300

    fix: add database

--- routes.js
+++ routes.js
@@ -1,35 +1,42 @@
 import { Router } from 'express';
+import knexfile from './knexfile.js';
+import knex from 'knex';
+import { randomUUID } from 'crypto';
 
 const routes = Router();
+const client = knex(knexfile);
 
-let lenMessages = 0
-const messages = []
 
 async function setMessage(req, res, next) {
     try {
       const { message } = req.body
       
-
       const data = {
-        id: lenMessages,
-        date: new Date(),
+        id: randomUUID(),
+        createdAt: new Date(),
         message,
         like: 0
       }
 
-      messages.push(data)
-      lenMessages++
-      
+      await client('messages')
+      .insert(data)
+      .catch((error) => {
+        throw new Error(error.detail);
+      });
+
       return res.status(200).json({});
   
     } catch (error) {
-      console.log(error.message)
-        next(error);
+      next(error);
     }
 }
 
 async function listMessages(req, res, next) {
   try {
+    const messages = await client.select('id', 'createdAt', 'message', 'like').from('messages').catch((error) => {
+      throw new Error(error.detail);
+    });
+
     return res.status(200).json({ messages });
   } catch (error) {
       next(error);
@@ -39,8 +46,9 @@ async function listMessages(req, res, next) {
 async function updatedMessageLikes(req, res, next) {
   try {
     const { id } = req.body
-    console.log(id)
-    messages[id].like = Number(messages[id].like) + 1
+    await client("messages").increment('like').where('id',id).catch((error) => {
+        throw new Error(error.detail);
+    });
     return res.status(200).json({});
 
   } catch (error) {
